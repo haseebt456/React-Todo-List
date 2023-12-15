d2ea4cec08bf74bf361de82745e6f86c66b10372
haseebt456 <m.haseebtufail@gmail.com>
2023-12-15 22:48:41 +0500

    Fix deletion using useState hook

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import "./App.css";
 import Header from "./components/Header";
 import Todos from "./components/Todos";
 import Footer from "./components/footer";
+import React, {useState} from "react";
 
 function App() {
 
   const onDelete = (todo)=>{
     console.log("I am on delete",todo);
-    
-    let index = todosArray.indexOf(todo);
-    todosArray.splice(index,1);
+    //item will delte but DOM will not update
+    //Deleting this way will not work in react
+    //let index = todosArray.indexOf(todo);
+    //todosArray.splice(index,1);
+    setTodos(todosArray.filter((elementToKeepInNewArray)=>{
+      return elementToKeepInNewArray !==todo;
+    }))
   }
-  let todosArray = [
+
+ /*  todosArray: This is the state variable that holds the current state of your to-do items. It is initialized with an array of three to-do items, each represented by an object.
+
+setTodos: This is the function that you can use to update the todosArray state. 
+In React, when you want to modify the state, you use the corresponding setter function provided by the useState hook.
+ In this case, setTodos would be used to update the todosArray state. */
+ 
+  const [todosArray, setTodos]= useState([
     {
       sno: 1,
       title: "Go to the university",
@@ -27,7 +39,7 @@ function App() {
       title: "complete your Assignments",
       desc: "You should go to complete your assignments by tomorrow"
     }
-  ]
+  ]);
   return (
     <>
       <Header title="My Todos list" serchBar={false} />
