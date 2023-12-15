1aab7a0576f46c18d8c5293085b008e014968751
haseebt456 <m.haseebtufail@gmail.com>
2023-12-15 22:26:58 +0500

    Fix key error

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -6,7 +6,7 @@ export default function Todos(props) {
   <div className="container">
     <h3 className="text-center my-3">Todos</h3>
     {props.todoslist.map((todo)=>{
-      return <TodoItem todo={todo} onDelete={props.onDelete}/> 
+      return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/> 
     })}
     
   </div>
