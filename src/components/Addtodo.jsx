7d1b0e8d96bff5c2f7744b70b518fa66cfdd312f
haseebt456 <m.haseebtufail@gmail.com>
2023-12-16 13:27:52 +0500

    Add submit function

--- src/components/Addtodo.jsx
+++ src/components/Addtodo.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Addtodo() {
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+
+    const submit = ()=>{
+
+    }
     return (
         <div className='container'>
             <h3>Add Todo</h3>
-            <form>
+            <form onSubmit={submit}>
                 <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
-                    <input type="text" class="form-control" id="description" aria-describedby="emailHelp" />
+                    <input type="text" value={title} class="form-control" id="description" aria-describedby="emailHelp" />
                 </div>
                 <div class="mb-3">
                     <label for="description" class="form-label">Description</label>
-                    <input type="text" class="form-control" id="description" />
+                    <input type="text" value={desc} class="form-control" id="desc" />
                 </div>
                 
                 <button type="submit" class="btn btn-success">Add todo</button>
