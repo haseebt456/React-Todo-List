980e6702f785611874a892ad27f4d4fef2e35c42
haseebt456 <m.haseebtufail@gmail.com>
2023-12-16 16:54:21 +0500

    create about page

---
+++ src/components/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h1>About Page</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia odio animi, vero velit ducimus hic cum, perferendis deleniti sit voluptatum nam, at praesentium et quibusdam ipsam. Eligendi consequatur aut doloremque.</p>
+    </div>
+  )
+}
